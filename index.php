bc791c366fb35dbae088fb204d8878685768f86d
Joel Lisenby <joel.lisenby@gmail.com>
2017-10-13 18:57:01 -0700

    Update index.php

--- index.php
+++ index.php
@@ -6,6 +6,6 @@ define('base_uri','/');
 
 // If the request URI starts with '/api/v1', instantiate the API class.
 if(substr($_SERVER['REQUEST_URI'],0,strlen(base_uri . api_uri)) === base_uri . api_uri) {
-	new \PMS\API;
+	new \API\API;
 }
 ?>
